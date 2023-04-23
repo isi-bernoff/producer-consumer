d83a25063c26c501ed594f35defbee709d6090ea
= <=>
2023-04-23 16:47:49 -0400

    Finish main.rs and start ledger.rs

---
+++ src/ledger.rs
@@ -0,0 +1,2 @@
+pub fn init_bank(_n: u8, _filename: String) {
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod ledger;
+
+use std::env::args;
+use std::process::exit;
+use ledger::init_bank;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    
+    if args.len() != 3 {
+        eprintln!("Usage: {} <num_of_threads> <leader_file>", args[0]);
+        exit(-1);
+    }
+
+    let n: u8 = args[1].parse().unwrap();
+    init_bank(n, args[2].clone());
 }
