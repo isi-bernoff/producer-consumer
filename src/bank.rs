28805bc8fbc6776932a9b4502f3ae265f93e37c7
= <=>
2023-05-03 13:24:59 -0400

    Write struct briefs and README.md

--- src/bank.rs
+++ src/bank.rs
@@ -3,19 +3,31 @@ use std::thread::Thread;
 use std::sync::Mutex;
 use std::fmt::{Formatter, Display, Result};
 
-enum Mode {
+enum Kind {
     Deposit,
-    Withdraw,
+    Withdrawal,
     Transfer
 }
 
+/// #### Fields:
+/// - `from_id`: The `u16` identifier of the account having its money removed
+/// - `to_id`: The `u16` identifier of the account having receiving money
+/// - `amount`: The `i32` amount of money being moved
+/// - `kind`: The `enum Kind` of transaction, a `Deposit`, `Withdrawal`, or `Transfer`
 struct Transaction {
     from_id: u16,
     to_id: u16,
     amount: i32,
-    mode: Mode
+    kind: Kind
 }
 
+/// #### Fields:
+/// - `threads`: A `HashMap` of `u16` identifiers to `Threads`
+/// - `accounts`: A `HashMap` of `u16` identifiers to `Mutex`-guarded `i32`
+///    balances
+/// - `ledger`: A `HashMap` of `u16` identifiers to `Transactions`
+/// - `num_successes`: The `u16` number of `Transaction`s that succeeded
+/// - `num_failures`: The `u16` number of `Transaction`s that failed
 pub struct Bank {
     threads: HashMap<u16, Thread>,
     accounts: HashMap<u16, Mutex<i32>>,
@@ -25,35 +37,33 @@ pub struct Bank {
 }
 
 impl Bank {
-    /// Constructs a new `Bank` object and initializes its `accounts`
-    /// #### Parameters:
-    /// `num_accounts` &ndash; The number of `Account`s to initialize
-    /// ### Returns:
+    /// Constructs a new `Bank` object and initializes `accounts`
+    /// #### Parameters
+    /// `num_accounts`: The number of `account`s to initialize
+    /// ### Returns
     /// The new `Bank` object
     pub fn new(num_accounts: u16) -> Self {
         todo!();
     }
     
     /// Initializes this `Bank`'s `threads` and `ledger`
-    /// #### Parameters:
-    /// `num_threads` &ndash; The number of `Thread`s to initialize \
-    /// `ledger_filename` &ndash; The name of a ledger file containing
-    /// transactions formatted `<from_id> <to_id> <amount> <mode>` on each line
-    pub fn init(num_threads: u16, ledger_filename: String) {
+    /// #### Parameters
+    /// - `num_threads`: The number of `Thread`s to initialize
+    /// - `ledger_filename`: The name of a ledger file containing transactions
+    /// formatted `<from_id> <to_id> <amount> <mode>` on each line
+    pub fn init(&self, num_threads: u16, ledger_filename: String) {
         todo!();
     }
     
     /// Deposit money into one of this `Bank`'s `accounts`, incrementing
-    /// `num_successes` or `num_failures` depending on \
-    /// if the deposit works
-    /// #### Parameters:
-    /// `thread_id` &ndash; The identifier of the `Thread` performing the the
-    /// deposit \
-    /// `deposit_id` &ndash; The identifier of the deposit `Transaction` \
-    /// `account_id` &ndash; The identifier of the account receiving the
-    /// deposit \
-    /// `amount` &ndash; The amount of money being deposited
-    /// #### Returns:
+    /// `num_successes` or `num_failures` depending on if the deposit works
+    /// #### Parameters
+    /// - `thread_id`: The identifier of the `Thread` processing the the
+    ///   deposit
+    /// - `deposit_id`: The identifier of the deposit `Transaction`
+    /// - `account_id`: The identifier of the account receiving the deposit
+    /// - `amount`: The amount of money being deposited
+    /// #### Returns
     /// A success message if the deposit succeeds and a failure message
     /// otherwise
     pub fn deposit(thread_id: u16, deposit_id: u16, account_id: u16,
@@ -62,16 +72,14 @@ impl Bank {
     }
 
     /// Withdraws money from one of this `Bank`'s `accounts`, incrementing
-    /// `num_successes` or `num_failures` depending on \
-    /// if the withdrawal works
-    /// #### Parameters:
-    /// `thread_id` &ndash; The identifier of the `Thread` performing the the
-    /// withdrawal \
-    /// `withdrawal_id` &ndash; The identifier of the withdrawal `Transaction` \
-    /// `account_id` &ndash; The identifier of the account having the
-    /// withdrawal \
-    /// `amount` &ndash; The amount of money being withdrawn
-    /// #### Returns:
+    /// `num_successes` or `num_failures` depending on if the withdrawal works
+    /// #### Parameters
+    /// - `thread_id`: The identifier of the `Thread` processing the the
+    ///   withdrawal
+    /// - `withdrawal_id`: The identifier of the withdrawal `Transaction`
+    /// - `account_id`: The identifier of the account having the withdrawal
+    /// - `amount`: The amount of money being withdrawn
+    /// #### Returns
     /// A success message if the withdrawal succeeds and a failure message
     /// otherwise
     pub fn withdraw(thread_id: u16, withdrawal_id: u16, account_id: u16,
@@ -80,18 +88,18 @@ impl Bank {
     }
 
     /// Transfers money from one of this `Bank`'s `accounts` to another,
-    /// incrementing `num_successes` or `num_failures` \
-    /// depending on if the transfer works
-    /// #### Parameters:
-    /// `thread_id` &ndash; The identifier of the `Thread` performing the the
-    /// transfer \
-    /// `transfer_id` &ndash; The identifier of the transfer `Transaction` \
-    /// `from_id` &ndash; The identifier of the account having money transferred
-    /// from it \
-    /// `to_id` &ndash; The identifier of the account having money transferred
-    /// to it \
-    /// `amount` &ndash; The amount of money being withdrawn
-    /// #### Returns:
+    /// incrementing `num_successes` or `num_failures` depending on if the
+    /// transfer works
+    /// #### Parameters
+    /// - `thread_id`: The identifier of the `Thread` processing the the
+    ///   transfer
+    /// - `transfer_id`: The identifier of the transfer `Transaction`
+    /// - `from_id`: The identifier of the account having money transferred
+    ///   from it
+    /// - `to_id`: The identifier of the account having money transferred
+    ///   to it \
+    /// - `amount`: The amount of money being withdrawn
+    /// #### Returns
     /// A success message if the transfer succeeds and a failure message
     /// otherwise
     pub fn transfer(thread_id: u16, transfer: u16, from_id: u16,
@@ -100,11 +108,11 @@ impl Bank {
     }
 
     /// Pops a `Transaction` from the `Bank`'s `ledger` and uses a `Thread`
-    /// to perform it
+    /// to process it
     /// concurrently
-    /// #### Parameters:
-    /// `id` &ndash; The identifier of the `Thread` performing the `Transaction`
-    pub fn init_thread(id: u16) {
+    /// #### Parameters
+    /// `id`: The identifier of the `Thread` processing the `Transaction`
+    pub fn thread(id: u16) {
         todo!();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![crate_name = "producer_consumer"]
+#![crate_name = "multi_bank"]
 
 use std::env::args;
 use std::process::exit;
@@ -16,5 +16,5 @@ fn main() {
     let num_threads: u16 = args[1].parse().unwrap();
     let ledger_filename: String = args[2].clone();
     let bank: Bank = Bank::new(10);
-    Bank::init(num_threads, ledger_filename);
+    bank.init(num_threads, ledger_filename);
 }
